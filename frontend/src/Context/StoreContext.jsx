afab3ff0e9a95b746b6f8694696be11a27e0e005
vincyproject <malvinocgv@gmail.com>
2024-11-08 10:11:00 +0700

    Testing

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://kaninara-backend.vercel.app/"
+    const url = "http://localhost:4000"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
