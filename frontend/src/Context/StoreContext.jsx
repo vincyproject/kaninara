f2190951c92dd88fce8d3a2d34783a6edaf74713
vincyproject <malvinocgv@gmail.com>
2024-11-13 20:54:00 +0700

    back to last

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-//
-    const url = "http://localhost:4000/"
+
+    const url = "https://kaninara-backend.vercel.app/"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
