a59597bb8669b44efca4463c81b1635b15c7095b
vincyproject <malvinocgv@gmail.com>
2024-11-13 20:29:36 +0700

    last commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://vincydigitalproject:Y5W3xpfbQz1MFvm7@kaninara.y0t1r.mongodb.net/').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://vincydigitalproject:P9dwc1LGICwlfAM8@kaninara.y0t1r.mongodb.net/').then(()=>console.log("DB Connected"));
     //mongodb+srv://vincyproject_1:6ZlssAM2hEWSDtaC@cluster0.omx4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 }
 
