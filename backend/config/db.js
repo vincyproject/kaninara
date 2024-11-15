1baf5233d64a8d85c37c8ad28c78b30d2748cf08
vincyproject <malvinocgv@gmail.com>
2024-11-15 13:44:24 +0700

    edit db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://vincydigitalproject:hc98Roo7uCo1FvVa@kaninara.13jw9.mongodb.net/').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://kaninara:kaninara@kaninara.omx4d.mongodb.net/').then(()=>console.log("DB Connected"));
     //mongodb+srv://vincyproject_1:6ZlssAM2hEWSDtaC@cluster0.omx4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 }
 
